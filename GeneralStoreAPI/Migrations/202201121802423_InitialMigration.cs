aeedaf19221828c1add2a5bfa79a1fb212ffafdd
AJ Hardin <ajhardin-dev@gmail.com>
2022-01-12 13:10:24 -0500

    Add project files.

---
+++ GeneralStoreAPI/Migrations/202201121802423_InitialMigration.cs
@@ -0,0 +1,28 @@
+﻿namespace GeneralStoreAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Sku = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        Cost = c.Double(nullable: false),
+                        NumberInInventory = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Sku);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
