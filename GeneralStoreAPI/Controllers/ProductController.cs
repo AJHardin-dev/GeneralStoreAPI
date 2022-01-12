faff3c69291f2b1e30b38e244b978c9cbb19823e
AJ Hardin <ajhardin-dev@gmail.com>
2022-01-12 14:12:22 -0500

    Starting on CRUD

---
+++ GeneralStoreAPI/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using GeneralStoreAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace GeneralStoreAPI.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private readonly ProductDbContext _context = new ProductDbContext();
+            
+        // api/Product
+        [HttpPost]
+        public async Task<IHttpActionResult> PostProduct([FromBody]Product model)
+        {
+            if (model == null)
+                return BadRequest("Can't be null");
+            else if (model != null && ModelState.IsValid)
+            {
+                _context.Products.Add(model);
+                await _context.SaveChangesAsync();
+
+                return Ok("Success");
+            }
+            else return BadRequest(ModelState);
+        }
+
+    }
+}
